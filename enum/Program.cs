64dad9f6ed94c6256214ac93023985d036d66def
Gabriel S. Olavo <gabrielsantosolavo101@gmail.com>
2024-08-17 20:31:20 -0300

    Novos comandos

---
+++ enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+
+    enum Dados 
+    {
+        Aventura, 
+        Acao,
+        Terror
+    };
+
+    static void Main(string[] args) 
+    {
+        // Variavel recebendo valor do enum
+        Dados genero = Dados.Aventura;
+
+        // Atribuindo pelo indice do enum
+        Dados genero2 = (Dados)2;
+
+        // Atribuindo para variavel explicita
+        var genero3 = (Dados)1;
+
+        Console.WriteLine(genero);
+        Console.WriteLine(genero2);
+        Console.WriteLine(genero3);
+    }
+}
